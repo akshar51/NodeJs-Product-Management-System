c3df79ce3f24e04513c9ee7b7fd5527a7afddf15
akshar51 <aksharparekh401@gmail.com>
2025-10-06 16:37:24 +0530

    done

---
+++ controller/productController.js
@@ -0,0 +1,7 @@
+module.exports.addProductPage = (req,res)=>{
+    res.render('pages/addProduct')
+}
+
+module.exports.tablePage = (req,res)=>{
+    res.render('pages/viewProduct')
+}
--- routers/index.routes.js
+++ routers/index.routes.js
@@ -1,11 +1,19 @@
 const { Router } = require("express");
 const dashboardCtl = require('../controller/dashboardController')
 const authCtl = require('../controller/authController')
+const pdtCtl = require('../controller/productController')
 const router = Router()
 
 router.get('/',dashboardCtl.homePage)
+
+// Auth
 router.get('/login',authCtl.loginPage)
 router.get('/register',authCtl.registerPage)
+
+// Product
+router.get('/addProduct',pdtCtl.addProductPage)
+router.get('/addProduct',pdtCtl.addProductPage)
+router.get('/table',pdtCtl.tablePage)
 
 
 module.exports = router
