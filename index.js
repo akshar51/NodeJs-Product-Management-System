bbc8a682ec1e8b34d03996e8a62e8e2ff65baf57
akshar51 <aksharparekh401@gmail.com>
2025-10-06 15:49:34 +0530

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const db = require('./configs/db');
+const port = 3000;
+const app = express()
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+
+app.listen(port,(err)=>{
+    if(!err){
+        db()
+        console.log("Server started..")
+        console.log("http://localhost:"+port)
+    }
+})
